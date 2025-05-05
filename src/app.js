c9a766c4b645c6f3eec25af6f14c5d7146757fa3
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2025-05-05 17:56:51 +0500

    added models

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 
 // Middleware to parse JSON data
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@socialbraindb.yisnycm.mongodb.net/?retryWrites=true&w=majority&appName=SocialBrainDb"
+  )
+  .then(async () => {
+    console.log("✅ Connected to MongoDB");
+    try {
+    } catch (err) {
+      console.log("Could not save test user:", err.message);
+    }
+  })
+  .catch((err) => {
+    console.error(" Connection error:", err);
+  });
 
 module.exports = app;
